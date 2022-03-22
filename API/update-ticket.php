71d4f4999736cc826ad6f83811746af97a1e01d7
maulanaiskandar <maulana@dartmedia.biz>
2022-03-22 10:03:06 +0700

    add validation isset http authorization

--- API/update-ticket.php
+++ API/update-ticket.php
@@ -4,13 +4,15 @@ header('Content-Type: application/json; charset=utf-8');
 require_once("../JWT/jwt.php");
 require_once("../config/dbconfig.php");
 require_once("../Utils/query.php");
+require_once("../Utils/utils.php");
 
 $jwt = new JWT();
+$query = new Query();
+$utils = new Utils();
 
 $database = new Database();
 $db = $database->connect_db("ticket_cli");
 
-$query = new Query();
 
 // validasi method api
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
@@ -18,42 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     exit();
 }
 
-function get_otorisasi_header(){
-    $headers = null;
-
-    if (isset($_SERVER['Authorization'])) {
-        $headers = trim($_SERVER["Authorization"]);
-    }
-    else if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
-        $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
-    } elseif (function_exists('apache_request_headers')) {
-        $requestHeaders = apache_request_headers();
-        
-        $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
-        
-        if (isset($requestHeaders['Authorization'])) {
-            $headers = trim($requestHeaders['Authorization']);
-        }
-    }
-
-    return $headers;
+// validasi cek header autherization
+if (!isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    http_response_code(401);
+    exit("Not Authenticated");
 }
 
-
-function get_token_bearer() {
-    $headers = get_otorisasi_header();
-    
-    if (!empty($headers)) {
-        if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
-            return $matches[1];
-        }
-    }else{
-        return null;
-    }
-}
-
-$token = get_token_bearer();
-
+// get token jwt 
+$token = $utils->get_token_bearer();
 $cek_token = $jwt->cek_jwt($token);
 
 // validasi cek bearer token
--- Utils/utils.php
+++ Utils/utils.php
@@ -2,7 +2,6 @@
 
 class Utils 
 {
-    protected $string;
 
     public function kodeTicket($prefix, $jml_char)
     {
@@ -13,5 +12,41 @@ class Utils
         }
 
         return $acak;
+    }
+
+    private function get_otorisasi_header()
+    {
+        $headers = null;
+    
+        if (isset($_SERVER['Authorization'])) {
+            $headers = trim($_SERVER["Authorization"]);
+        }
+        else if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+        } elseif (function_exists('apache_request_headers')) {
+            $requestHeaders = apache_request_headers();
+            
+            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+            
+            if (isset($requestHeaders['Authorization'])) {
+                $headers = trim($requestHeaders['Authorization']);
+            }
+        }
+    
+        return $headers;
+    }
+    
+    
+    public function get_token_bearer() 
+    {
+        $headers = $this->get_otorisasi_header();
+        
+        if (!empty($headers)) {
+            if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+                return $matches[1];
+            }
+        }else{
+            return null;
+        }
     }
 }
